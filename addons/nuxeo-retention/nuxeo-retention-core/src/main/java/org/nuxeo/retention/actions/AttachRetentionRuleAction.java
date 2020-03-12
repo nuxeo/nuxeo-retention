nuxeo/nuxeo

NXP-27692: add retention addon

addons/nuxeo-retention/nuxeo-retention-core/src/main/java/org/nuxeo/retention/actions/AttachRetentionRuleAction.java
@@ -1,0 +1,102 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume RENARD
+ */
+package org.nuxeo.retention.actions;
+
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.bulk.action.computation.AbstractBulkComputation;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
+import org.nuxeo.ecm.platform.audit.service.NXAuditEventsService;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.retention.adapters.RetentionRule;
+import org.nuxeo.retention.service.RetentionManager;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * Bulk action to attach retention rule.
+ * @since 11.1
+ */
+public class AttachRetentionRuleAction implements StreamProcessorTopology {
+
+    public static final String ACTION_NAME = "attachRetentionRule";
+
+    public static final String PARAM_RULE_ID = "ruleId";
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        return Topology.builder()
+                       .addComputation(EvaluateRuleComputation::new,
+                               Arrays.asList(INPUT_1 + ":" + ACTION_NAME, OUTPUT_1 + ":" + STATUS_STREAM))
+                       .build();
+    }
+
+    public static class EvaluateRuleComputation extends AbstractBulkComputation {
+
+        static final Logger log = LogManager.getLogger(EvaluateRuleComputation.class);
+
+        protected boolean disableAudit;
+
+        protected RetentionManager retentionManager;
+
+        protected String ruleId;
+
+        public EvaluateRuleComputation() {
+            super(ACTION_NAME);
+        }
+
+        @Override
+        public void startBucket(String bucketKey) {
+            BulkCommand command = getCurrentCommand();
+            Serializable auditParam = command.getParam(NXAuditEventsService.DISABLE_AUDIT_LOGGER);
+            disableAudit = auditParam != null && Boolean.parseBoolean(auditParam.toString());
+            retentionManager = Framework.getService(RetentionManager.class);
+            ruleId = (String) command.getParam(PARAM_RULE_ID);
+        }
+
+        @Override
+        protected void compute(CoreSession session, List<String> ids, Map<String, Serializable> properties) {
+            // Attach rule
+            DocumentModel ruleDoc = session.getDocument(new IdRef(ruleId));
+            RetentionRule rule = ruleDoc.getAdapter(RetentionRule.class);
+            for (DocumentModel doc : loadDocuments(session, ids)) {
+                if (!retentionManager.canAttachRule(doc, rule, session)) {
+                    continue;
+                }
+                if (disableAudit) {
+                    doc.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, Boolean.TRUE);
+                }
+                retentionManager.attachRule(doc, rule, session);
+            }
+        }
+    }
+
+}

addons/nuxeo-retention/nuxeo-retention-core/src/main/java/org/nuxeo/retention/adapters/Record.java
@@ -1,0 +1,139 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume RENARD
+ */
+package org.nuxeo.retention.adapters;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.versioning.VersioningService;
+import org.nuxeo.ecm.platform.audit.service.NXAuditEventsService;
+import org.nuxeo.ecm.platform.dublincore.listener.DublinCoreListener;
+import org.nuxeo.ecm.platform.ec.notification.NotificationConstants;
+import org.nuxeo.retention.RetentionConstants;
+
+/**
+ * @since 11.1
+ */
+public class Record {
+
+    private static final Logger log = LogManager.getLogger(Record.class);
+
+    protected DocumentModel document;
+
+    public Record(final DocumentModel doc) {
+        document = doc;
+    }
+
+    public DocumentModel getDocument() {
+        return document;
+    }
+
+    public RetentionRule getRule(CoreSession session) {
+        List<String> ruleIds = getRuleIds();
+        if (ruleIds == null) {
+            return null;
+        }
+        for (String ruleId : ruleIds) {
+            IdRef ruleRef = new IdRef(ruleId);
+            if (!session.exists(ruleRef)) {
+                log.trace("Rule {} does not exist", ruleRef);
+                continue;
+            }
+            DocumentModel ruleDoc = session.getDocument(ruleRef);
+            RetentionRule rule = ruleDoc.getAdapter(RetentionRule.class);
+            if (!rule.isEnabled()) {
+                log.debug("Rule {} id disabled", ruleDoc::getPathAsString);
+                continue;
+            }
+            return rule;
+        }
+        log.debug("No active rules found for {}", document::getPathAsString);
+        return null;
+
+    }
+
+    protected List<String> getRuleIds() {
+        Serializable propertyValue = document.getPropertyValue(RetentionConstants.RECORD_RULE_IDS_PROP);
+        if (propertyValue == null) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList((String[]) propertyValue);
+    }
+
+    public Calendar getSavedRetainUntil() {
+        Serializable savedRetainUntil = document.getPropertyValue(RetentionConstants.RETAIN_UNTIL_PROP);
+        if (savedRetainUntil != null) {
+            return (Calendar) savedRetainUntil;
+        }
+        return null;
+    }
+
+    public boolean isRetainUntilInderterminate() {
+        if (!getDocument().isUnderRetentionOrLegalHold()) {
+            return false;
+        }
+        Calendar retainUntil = getDocument().getRetainUntil();
+        return retainUntil != null
+                ? CoreSession.RETAIN_UNTIL_INDETERMINATE.getTimeInMillis() == retainUntil.getTimeInMillis()
+                : false;
+    }
+
+    public boolean isRetentionExpired() {
+        if (!getDocument().isUnderRetentionOrLegalHold()) {
+            return true;
+        }
+        Calendar retainUntil;
+        return (retainUntil = getDocument().getRetainUntil()) == null || !Calendar.getInstance().before(retainUntil);
+    }
+
+    protected void save(CoreSession session) {
+        document.putContextData(DublinCoreListener.DISABLE_DUBLINCORE_LISTENER, true);
+        document.putContextData(NotificationConstants.DISABLE_NOTIFICATION_SERVICE, true);
+        document.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, true);
+        document.putContextData(VersioningService.DISABLE_AUTO_CHECKOUT, true);
+        document.putContextData(RetentionConstants.RETENTION_CHECKER_LISTENER_IGNORE, true);
+        session.saveDocument(document);
+        document.putContextData(DublinCoreListener.DISABLE_DUBLINCORE_LISTENER, null);
+        document.putContextData(NotificationConstants.DISABLE_NOTIFICATION_SERVICE, null);
+        document.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, null);
+        document.putContextData(VersioningService.DISABLE_AUTO_CHECKOUT, null);
+        document.getContextData().remove(RetentionConstants.RETENTION_CHECKER_LISTENER_IGNORE);
+    }
+
+    public void saveRetainUntil(Calendar retainUntil) {
+        document.setPropertyValue(RetentionConstants.RETAIN_UNTIL_PROP, retainUntil);
+    }
+
+    public void setRule(RetentionRule rule, CoreSession session) {
+        setRuleIds(Arrays.asList(rule.getDocument().getId()));
+        save(session);
+    }
+
+    protected void setRuleIds(final List<String> ruleIds) {
+        document.setPropertyValue(RetentionConstants.RECORD_RULE_IDS_PROP, (Serializable) ruleIds);
+    }
+}

nuxeo/nuxeo-retention

addons/nuxeo-retention/nuxeo-retention-core/src/main/java/org/nuxeo/retention/actions/AttachRetentionRuleAction.java
@@ -1,0 +1,98 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume RENARD
+ */
+package org.nuxeo.retention.actions;
+
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.bulk.action.computation.AbstractBulkComputation;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
+import org.nuxeo.ecm.platform.audit.service.NXAuditEventsService;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.retention.adapters.RetentionRule;
+import org.nuxeo.retention.service.RetentionManager;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * Bulk action to attach a retention rule.
+ *
+ * @since 11.1
+ */
+public class AttachRetentionRuleAction implements StreamProcessorTopology {
+
+    public static final String ACTION_NAME = "attachRetentionRule";
+
+    public static final String PARAM_RULE_ID = "ruleId";
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        return Topology.builder()
+                       .addComputation(EvaluateRuleComputation::new,
+                               List.of(INPUT_1 + ":" + ACTION_NAME, OUTPUT_1 + ":" + STATUS_STREAM))
+                       .build();
+    }
+
+    public static class EvaluateRuleComputation extends AbstractBulkComputation {
+
+        protected boolean disableAudit;
+
+        protected RetentionManager retentionManager;
+
+        protected String ruleId;
+
+        public EvaluateRuleComputation() {
+            super(ACTION_NAME);
+        }
+
+        @Override
+        public void startBucket(String bucketKey) {
+            BulkCommand command = getCurrentCommand();
+            Serializable auditParam = command.getParam(NXAuditEventsService.DISABLE_AUDIT_LOGGER);
+            disableAudit = auditParam != null && Boolean.parseBoolean(auditParam.toString());
+            retentionManager = Framework.getService(RetentionManager.class);
+            ruleId = command.getParam(PARAM_RULE_ID);
+        }
+
+        @Override
+        protected void compute(CoreSession session, List<String> ids, Map<String, Serializable> properties) {
+            // Attach rule
+            DocumentModel ruleDoc = session.getDocument(new IdRef(ruleId));
+            RetentionRule rule = ruleDoc.getAdapter(RetentionRule.class);
+            for (DocumentModel doc : loadDocuments(session, ids)) {
+                if (!retentionManager.canAttachRule(doc, rule, session)) {
+                    continue;
+                }
+                if (disableAudit) {
+                    doc.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, Boolean.TRUE);
+                }
+                retentionManager.attachRule(doc, rule, session);
+            }
+        }
+    }
+
+}

addons/nuxeo-retention/nuxeo-retention-core/src/main/java/org/nuxeo/retention/adapters/Record.java
@@ -1,0 +1,141 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume RENARD
+ */
+package org.nuxeo.retention.adapters;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.versioning.VersioningService;
+import org.nuxeo.ecm.platform.audit.service.NXAuditEventsService;
+import org.nuxeo.ecm.platform.dublincore.listener.DublinCoreListener;
+import org.nuxeo.ecm.platform.ec.notification.NotificationConstants;
+import org.nuxeo.retention.RetentionConstants;
+
+/**
+ * @since 11.1
+ */
+public class Record {
+
+    private static final Logger log = LogManager.getLogger(Record.class);
+
+    protected DocumentModel document;
+
+    public Record(final DocumentModel doc) {
+        document = doc;
+    }
+
+    public DocumentModel getDocument() {
+        return document;
+    }
+
+    public RetentionRule getRule(CoreSession session) {
+        List<String> ruleIds = getRuleIds();
+        if (ruleIds == null) {
+            return null;
+        }
+        for (String ruleId : ruleIds) {
+            IdRef ruleRef = new IdRef(ruleId);
+            if (!session.exists(ruleRef)) {
+                log.trace("Rule {} does not exist", ruleRef);
+                continue;
+            }
+            DocumentModel ruleDoc = session.getDocument(ruleRef);
+            RetentionRule rule = ruleDoc.getAdapter(RetentionRule.class);
+            if (!rule.isEnabled()) {
+                log.debug("Rule {} id disabled", ruleDoc::getPathAsString);
+                continue;
+            }
+            return rule;
+        }
+        log.debug("No active rules found for {}", document::getPathAsString);
+        return null;
+
+    }
+
+    protected List<String> getRuleIds() {
+        Serializable propertyValue = document.getPropertyValue(RetentionConstants.RECORD_RULE_IDS_PROP);
+        if (propertyValue == null) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList((String[]) propertyValue);
+    }
+
+    public Calendar getSavedRetainUntil() {
+        Serializable savedRetainUntil = document.getPropertyValue(RetentionConstants.RETAIN_UNTIL_PROP);
+        if (savedRetainUntil != null) {
+            return (Calendar) savedRetainUntil;
+        }
+        return null;
+    }
+
+    public boolean isRetentionIndeterminate() {
+        if (!getDocument().isUnderRetentionOrLegalHold()) {
+            return false;
+        }
+        Calendar retainUntil = getDocument().getRetainUntil();
+        if (retainUntil == null) {
+            return false;
+        }
+        return retainUntil.compareTo(CoreSession.RETAIN_UNTIL_INDETERMINATE) == 0;
+    }
+
+    public boolean isRetentionExpired() {
+        if (!getDocument().isUnderRetentionOrLegalHold()) {
+            return true;
+        }
+        Calendar retainUntil;
+        return (retainUntil = getDocument().getRetainUntil()) == null || !Calendar.getInstance().before(retainUntil);
+    }
+
+    protected void save(CoreSession session) {
+        document.putContextData(DublinCoreListener.DISABLE_DUBLINCORE_LISTENER, true);
+        document.putContextData(NotificationConstants.DISABLE_NOTIFICATION_SERVICE, true);
+        document.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, true);
+        document.putContextData(VersioningService.DISABLE_AUTO_CHECKOUT, true);
+        document.putContextData(RetentionConstants.RETENTION_CHECKER_LISTENER_IGNORE, true);
+        session.saveDocument(document);
+        document.putContextData(DublinCoreListener.DISABLE_DUBLINCORE_LISTENER, null);
+        document.putContextData(NotificationConstants.DISABLE_NOTIFICATION_SERVICE, null);
+        document.putContextData(NXAuditEventsService.DISABLE_AUDIT_LOGGER, null);
+        document.putContextData(VersioningService.DISABLE_AUTO_CHECKOUT, null);
+        document.getContextData().remove(RetentionConstants.RETENTION_CHECKER_LISTENER_IGNORE);
+    }
+
+    public void saveRetainUntil(Calendar retainUntil) {
+        document.setPropertyValue(RetentionConstants.RETAIN_UNTIL_PROP, retainUntil);
+    }
+
+    public void setRule(RetentionRule rule, CoreSession session) {
+        setRuleIds(Arrays.asList(rule.getDocument().getId()));
+        save(session);
+    }
+
+    protected void setRuleIds(final List<String> ruleIds) {
+        document.setPropertyValue(RetentionConstants.RECORD_RULE_IDS_PROP, (Serializable) ruleIds);
+    }
+
+}