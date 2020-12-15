5f2e9a92c4dc4c0ad600b669d0a5551c6af668bc
Abdoul BA <aba@nuxeo.com>
2020-12-15 14:41:57 +0100

    NXP-29981: cleanup and format

--- ci/jenkinsfiles/build.groovy
+++ ci/jenkinsfiles/build.groovy
@@ -313,11 +313,11 @@ pipeline {
         stage('Git Push') {
           steps {
             container('maven') {
-              echo '''
+              echo """
                 --------------------------
                 Git push ${TAG}
                 --------------------------
-              '''
+              """
               script {
                 pipelineLib.gitPush("${TAG}")
               }
