d4b77621295ebd7b6ac606e0c1f8c2cbbfaa9a53
Andrea Cornaglia <acornaglia@nuxeo.com>
2021-07-06 15:59:26 +0200

    NXP-28809: update preprod clientId

--- ci/jenkinsfiles/build.groovy
+++ ci/jenkinsfiles/build.groovy
@@ -71,7 +71,7 @@ pipeline {
     JENKINS_HOME = '/root'
     MAVEN_DEBUG = '-e'
     MAVEN_OPTS = "${MAVEN_OPTS} -Xms512m -Xmx3072m"
-    NUXEO_BASE_IMAGE = 'docker-private.packages.nuxeo.com/nuxeo/nuxeo:11.4.42'
+    NUXEO_BASE_IMAGE = 'docker-private.packages.nuxeo.com/nuxeo/nuxeo:11.5.141'
     ORG = 'nuxeo'
     PREVIEW_NAMESPACE = "retention-${BRANCH_LC}"
     REFERENCE_BRANCH = 'master'
