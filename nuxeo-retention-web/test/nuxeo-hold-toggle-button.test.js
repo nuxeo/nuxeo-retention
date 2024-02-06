c706ec639d0e8d0dbb8c6412541ee4a5b7daa8d9
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-02-06 11:50:42 +0530

    NXP-32298: configure karma and npm modules setup to run frontend unit tests

---
+++ nuxeo-retention-web/test/nuxeo-hold-toggle-button.test.js
@@ -0,0 +1,100 @@
+/**
+@license
+©2023 Hyland Software, Inc. and its affiliates. All rights reserved. 
+All Hyland product names are registered or unregistered trademarks of Hyland Software, Inc. or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { fixture, html, login } from '@nuxeo/testing-helpers';
+import '../elements/nuxeo-hold-toggle-button.js';
+
+const document = {
+  'entity-type': 'document',
+  contextParameters: {
+    breadcrumb: {
+      entries: [
+        {
+          path: '/default-domain',
+          title: 'Domain',
+          type: 'Domain',
+          uid: '1',
+        },
+        {
+          path: '/default-domain/workspaces',
+          title: 'Workspaces',
+          type: 'WorkspaceRoot',
+          uid: '2',
+        },
+        {
+          path: '/default-domain/workspaces/my workspace',
+          title: 'my workspace',
+          type: 'Workspace',
+          uid: '3',
+        },
+        {
+          path: '/default-domain/workspaces/my workspace/folder 1',
+          title: 'folder 1',
+          type: 'Folder',
+          uid: '4',
+        },
+        {
+          path: '/default-domain/workspaces/my workspace/folder 1/folder 2',
+          title: 'folder 2',
+          type: 'Folder',
+          uid: '5',
+        },
+        {
+          path: '/default-domain/workspaces/my workspace/folder 1/folder 2/folder 3',
+          title: 'folder 3',
+          type: 'Folder',
+          uid: '6',
+        },
+        {
+          path: '/default-domain/workspaces/my workspace/folder 1/folder 2/folder 3/my file',
+          title: 'my file',
+          type: 'File',
+          uid: '7',
+        },
+      ],
+    },
+  },
+  path: '/default-domain/workspaces/my workspace/folder 1/folder 2/folder 3/my file',
+  title: 'my file',
+  type: 'File',
+  uid: '7',
+};
+
+suite('nuxeo-hold-toggle-button', () => {
+  let server;
+  let breadcrumb;
+
+  setup(async () => {
+    server = await login();
+    breadcrumb = await fixture(
+      html`
+        <nuxeo-hold-toggle-button .document=${document}></nuxeo-hold-toggle-button>
+      `,
+    );
+  });
+
+  teardown(() => {
+    server.restore();
+  });
+
+  suite('Visibility', () => {
+    test('Should display the document title when a document is set and has a title', async () => {
+      
+    });
+  });
+
+});
