630eb691853856c11033942734223f2901d2faf5
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-02-28 16:49:35 +0530

    test retention ftest

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -154,8 +154,6 @@ pipeline {
             }
           }
           nxWithGitHubStatus(context: 'ftests') {
-            echo "Functional tests are disabled (https://jira.nuxeo.com/browse/WEBUI-1260 and https://jira.nuxeo.com/browse/WEBUI-1252)"
-            /*
             script {
               def testNamespace = "${CURRENT_NAMESPACE}-retention-${BRANCH_NAME}-${BUILD_NUMBER}-ftests".replaceAll('\\.', '-').toLowerCase()
               def nuxeoParentVersion = readMavenPom().getParent().getVersion()
@@ -169,7 +167,6 @@ pipeline {
                 }
               }
             }
-            */
           }
         }
       }
