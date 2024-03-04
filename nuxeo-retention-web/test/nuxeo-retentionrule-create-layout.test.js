8f177e56ab6c895475d11d73b690a360661a048d
swarnadipa-dev <swarnadipa.choudhury@globallogic.com>
2024-03-04 15:25:32 +0530

    commit 1

---
+++ nuxeo-retention-web/test/nuxeo-retentionrule-create-layout.test.js
@@ -0,0 +1,33 @@
+/**
+@license
+©2023 Hyland Software, Inc. and its affiliates. All rights reserved. 
+All Hyland product names are registered or unregistered trademarks of Hyland Software, Inc. or its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use createEl file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import '../document/retentionrule/nuxeo-retentionrule-create-layout.html';
+// import '../elements/nuxeo-attach-rule-button.js';
+
+suite('nuxeo-retentionrule-create-layout', () => {
+  // let createEl;
+
+  setup(async () => {
+    //  createEl = await fixture(html` <nuxeo-retentionrule-create-layout .document=${document}></nuxeo-retentionrule-create-layout> `);
+  });
+
+  suite('test validate', () => {
+    test('Should return provider object if provider is available', async () => {
+      //    expect(createEl.validate()).equal(false);
+    });
+  });
+});
