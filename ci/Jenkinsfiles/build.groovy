e9effeb4a7b20ca83184d4934ce65927b5e530f4
Kevin Leturc <kevin.leturc@hyland.com>
2024-09-04 10:04:35 +0200

    NXBT-3821: Prepare Helmfile upgrade

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -16,7 +16,7 @@
 * Contributors:
 *     Kevin Leturc <kevin.leturc@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.25"
+library identifier: "platform-ci-shared-library@v0.0.39"
 
 Closure buildUnitTestStage(env) {
   return {
