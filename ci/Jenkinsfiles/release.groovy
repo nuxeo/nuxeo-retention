42460614a15072d40e37d58f5b5e47d1d5e5e3e0
Guillaume Renard <guillaume.renard@hyland.com>
2025-03-14 10:18:24 +0100

    NXBT-3857: Use shared library to release the project

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -19,7 +19,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.39"
+library identifier: "platform-ci-shared-library@v0.0.47"
 
 pipeline {
   agent {
@@ -33,7 +33,9 @@ pipeline {
   environment {
     BRANCH_NAME = "${params.BRANCH}"
     BUILD_VERSION = "${params.BUILD_VERSION}"
-    JIRA_NUXEO_ADDON_MOVING_VERSION = 'retention-2025.x'
+    JIRA_PROJECT = 'NXP'
+    JIRA_MOVING_VERSION = 'retention-2025.x'
+    JIRA_RELEASED_VERSION = "retention-${VERSION}"
     VERSION = "${nxUtils.getMajorDotMinorVersion(version: env.BUILD_VERSION)}"
   }
   stages {
@@ -95,24 +97,19 @@ pipeline {
       steps {
         container('maven') {
           script {
-            def jiraVersionName = "retention-${VERSION}"
-            // create a new released version in Jira
-            def jiraVersion = [
-                project: 'NXP',
-                name: jiraVersionName,
-                description: "Retention Addon ${VERSION}",
-                releaseDate: LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE),
-                released: true,
+            def jiraIssueFetchers = [
+                type                 : 'jira',
+                jql                  : "project = ${JIRA_PROJECT} and fixVersion = ${JIRA_MOVING_VERSION}",
+                newJiraVersion       : [
+                    project    : env.JIRA_PROJECT,
+                    name       : env.JIRA_RELEASED_VERSION,
+                    description: "Retention Addon ${VERSION}",
+                    releaseDate: LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE),
+                    released   : true,
+                ],
+                jiraMovingVersionName: env.JIRA_MOVING_VERSION,
             ]
-            nxJira.newVersion(version: jiraVersion)
-            // find Jira tickets included in this release and update them
-            def jiraTickets = nxJira.jqlSearch(jql: "project = NXP and fixVersion = ${JIRA_NUXEO_ADDON_MOVING_VERSION}")
-            def previousVersion = nxUtils.getPreviousMajorDotMinorVersion()
-            def changelog = nxGit.getChangeLog(previousVersion: previousVersion, version: env.VERSION)
-            def committedIssues = jiraTickets.data.issues.findAll { changelog.contains(it.key) }
-            committedIssues.each {
-              nxJira.editIssueFixVersion(idOrKey: it.key, fixVersionToRemove: env.JIRA_NUXEO_ADDON_MOVING_VERSION, fixVersionToAdd: jiraVersionName)
-            }
+            nxProject.release(issuesFetchers: [jiraIssueFetchers], previousVersion: nxUtils.getPreviousMajorDotMinorVersion())
           }
         }
       }
