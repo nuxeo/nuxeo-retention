15c03824adb72ccd72bfb4a23feaaad61cd95d1b
Antoine Taillefer <ataillefer@nuxeo.com>
2025-05-12 15:42:59 +0200

    NXBT-3870: Update code to leverage Dependabot fetcher for Release Notes

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -97,19 +97,17 @@ pipeline {
       steps {
         container('maven') {
           script {
-            def jiraIssueFetchers = [
-                type                 : 'jira',
-                jql                  : "project = ${JIRA_PROJECT} and fixVersion = ${JIRA_MOVING_VERSION}",
-                newJiraVersion       : [
-                    project    : env.JIRA_PROJECT,
-                    name       : env.JIRA_RELEASED_VERSION,
-                    description: "Retention Addon ${VERSION}",
-                    releaseDate: LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE),
-                    released   : true,
-                ],
-                jiraMovingVersionName: env.JIRA_MOVING_VERSION,
-            ]
-            nxProject.release(issuesFetchers: [jiraIssueFetchers], previousVersion: nxUtils.getPreviousMajorDotMinorVersion())
+            nxProject.release(
+              jql                  : "project = ${JIRA_PROJECT} and fixVersion = ${JIRA_MOVING_VERSION}",
+              newJiraVersion       : [
+                project    : env.JIRA_PROJECT,
+                name       : env.JIRA_RELEASED_VERSION,
+                description: "Retention Addon ${VERSION}",
+                releaseDate: LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE),
+                released   : true,
+              ],
+              jiraMovingVersionName: env.JIRA_MOVING_VERSION,
+            )
           }
         }
       }
