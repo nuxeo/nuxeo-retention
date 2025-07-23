79123b7471154d8f12df1c40056970523caeba7f
Nishant0928 <Nishant.Singh@contractors.onbase.com>
2025-07-23 19:56:41 +0530

    NXP-33200: upgrading node version and webui package in retention repo

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -193,7 +193,7 @@ pipeline {
               def testNamespace = "${CURRENT_NAMESPACE}-retention-${BRANCH_NAME}-${BUILD_NUMBER}-ftests".replaceAll('\\.', '-').toLowerCase()
               def nuxeoParentVersion = readMavenPom().getParent().getVersion()
               // target connect preprod if nuxeo-parent is a snapshot version or a build version
-              def clidSecret = 'instance-clid-preprod'
+              def clidSecret = nuxeoParentVersion.matches("^\\d+\\.\\d+(-SNAPSHOT|\\.\\d+)\$") ? 'instance-clid-preprod' : 'instance-clid'
               nxWithHelmfileDeployment(namespace: testNamespace, environment: "functionalTests", envVars: ["CONNECT_CLID_SECRET=${clidSecret}"],
                   secrets: [[name: clidSecret, namespace: 'platform']]) {
                 dir('nuxeo-retention-web') {
