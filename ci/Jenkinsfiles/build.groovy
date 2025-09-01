72450c5efc602e50cc04eee29be01216f625a235
Kevin Leturc <kevin.leturc@hyland.com>
2025-09-01 14:17:39 +0200

    NXBT-3889: Leverage RUN --mount bind capability to install packages

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -62,6 +62,7 @@ pipeline {
   environment {
     CURRENT_NAMESPACE = nxK8s.getCurrentNamespace()
     MAVEN_OPTS = "$MAVEN_OPTS -Xms512m -Xmx3072m"
+    NUXEO_VERSION = nxMvn.getProperty(key: 'project.parent.version')
     VERSION = nxUtils.getVersion()
     NUXEO_RETENTION_PACKAGE_PATH = "nuxeo-retention-package/target/nuxeo-retention-package-${VERSION}.zip"
   }
@@ -151,20 +152,22 @@ pipeline {
     stage('Run functional tests') {
       steps {
         container('maven') {
-          nxWithGitHubStatus(context: 'docker/build') {
-            script {
+          script {
+            // target connect preprod if nuxeo-parent is a snapshot version or a build version
+            def clidSecret = env.NUXEO_VERSION.matches("^\\d+\\.\\d+(-SNAPSHOT|\\.\\d+)\$") ? 'instance-clid-preprod' : 'instance-clid'
+            def clid = nxK8s.getSecretData(namespace: 'platform', name: clidSecret, key: 'instance\\.clid')
+            def connectUrl = clidSecret.contains('preprod') ? CONNECT_PREPROD_SITE_URL : CONNECT_PROD_SITE_URL
+
+            nxWithGitHubStatus(context: 'docker/build') {
               sh "mkdir -p ci/docker/target && cp ${NUXEO_RETENTION_PACKAGE_PATH} ci/docker/target"
-              def nuxeoVersion = sh(returnStdout: true,
-                  script: 'mvn org.apache.maven.plugins:maven-help-plugin:3.3.0:evaluate -Dexpression=nuxeo.platform.version -q -DforceStdout')
-              nxDocker.build(skaffoldFile: 'ci/docker/skaffold.yaml', envVars: ["NUXEO_VERSION=${nuxeoVersion}"])
+              def nuxeoVersion = nxMvn.getProperty(key: 'nuxeo.platform.version')
+              // use withEnv for clid to not print it to the console, which nxDocker does
+              withEnv(["CLID=${clid}"]) {
+                nxDocker.build(skaffoldFile: 'ci/docker/skaffold.yaml', envVars: ["CONNECT_URL=${connectUrl}", "NUXEO_VERSION=${nuxeoVersion}"])
+              }
             }
-          }
-          nxWithGitHubStatus(context: 'ftests') {
-            script {
+            nxWithGitHubStatus(context: 'ftests') {
               def testNamespace = "${CURRENT_NAMESPACE}-retention-${BRANCH_NAME}-${BUILD_NUMBER}-ftests".replaceAll('\\.', '-').toLowerCase()
-              def nuxeoParentVersion = readMavenPom().getParent().getVersion()
-              // target connect preprod if nuxeo-parent is a snapshot version or a build version
-              def clidSecret = nuxeoParentVersion.matches("^\\d+\\.\\d+(-SNAPSHOT|\\.\\d+)\$") ? 'instance-clid-preprod' : 'instance-clid'
               nxWithHelmfileDeployment(namespace: testNamespace, environment: "functionalTests", envVars: ["CONNECT_CLID_SECRET=${clidSecret}"],
                   secrets: [[name: clidSecret, namespace: 'platform']]) {
                 dir('nuxeo-retention-web') {
