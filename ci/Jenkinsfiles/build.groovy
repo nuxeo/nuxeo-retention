58b43e8071d02274a201984dbc5bd8530e748916
Antoine Taillefer <ataillefer@nuxeo.com>
2025-11-03 17:09:37 +0100

    NXP-33390: Align nuxeo-web-ui package version on npm dependencies

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -18,6 +18,19 @@
 */
 library identifier: "platform-ci-shared-library@v0.0.75"
 
+String getWebUIVersion() {
+  container('maven') {
+    dir('nuxeo-retention-web') {
+      return sh(
+        returnStdout: true,
+        script: '''
+          jq --raw-output '.packages."node_modules/@nuxeo/nuxeo-web-ui-ftest".version' package-lock.json
+        '''
+      ).trim()
+    }
+  }
+}
+
 Closure buildUnitTestStage(env) {
   return {
     container('maven') {
@@ -66,6 +79,7 @@ pipeline {
     NUXEO_VERSION = nxMvn.getProperty(key: 'project.parent.version')
     VERSION = nxUtils.getVersion()
     NUXEO_RETENTION_PACKAGE_PATH = "nuxeo-retention-package/target/nuxeo-retention-package-${VERSION}.zip"
+    NUXEO_WEB_UI_VERSION = getWebUIVersion()
   }
   stages {
     stage('Set labels') {
@@ -201,13 +215,21 @@ pipeline {
             def clidSecret = env.NUXEO_VERSION.matches("^\\d+\\.\\d+(-SNAPSHOT|\\.\\d+)\$") ? 'instance-clid-preprod' : 'instance-clid'
             def clid = nxK8s.getSecretData(namespace: 'platform', name: clidSecret, key: 'instance\\.clid')
             def connectUrl = clidSecret.contains('preprod') ? CONNECT_PREPROD_SITE_URL : CONNECT_PROD_SITE_URL
+            def nuxeoWebUIPackage = "nuxeo-web-ui-${NUXEO_WEB_UI_VERSION}"
 
             nxWithGitHubStatus(context: 'docker/build') {
               sh "mkdir -p ci/docker/target && cp ${NUXEO_RETENTION_PACKAGE_PATH} ci/docker/target"
               def nuxeoVersion = nxMvn.getProperty(key: 'nuxeo.platform.version')
               // use withEnv for clid to not print it to the console, which nxDocker does
               withEnv(["CLID=${clid}"]) {
-                nxDocker.build(skaffoldFile: 'ci/docker/skaffold.yaml', envVars: ["CONNECT_URL=${connectUrl}", "NUXEO_VERSION=${nuxeoVersion}"])
+                nxDocker.build(
+                  skaffoldFile: 'ci/docker/skaffold.yaml',
+                  envVars: [
+                    "CONNECT_URL=${connectUrl}",
+                    "NUXEO_VERSION=${nuxeoVersion}",
+                    "NUXEO_WEB_UI_PACKAGE=${nuxeoWebUIPackage}"
+                  ]
+                )
               }
             }
             nxWithGitHubStatus(context: 'ftests') {
