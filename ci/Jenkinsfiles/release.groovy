a001aed9b6fbee62a92ca90eff05c90cc5dee229
Antoine Taillefer <ataillefer@nuxeo.com>
2026-03-10 12:16:49 +0100

    NXBT-3951: Remove unneeded options duplicating job definition

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -25,11 +25,6 @@ pipeline {
   agent {
     label 'jenkins-nuxeo-package-lts-2023'
   }
-  options {
-    buildDiscarder(logRotator(daysToKeepStr: '60', numToKeepStr: '60', artifactNumToKeepStr: '5'))
-    disableConcurrentBuilds()
-    githubProjectProperty(projectUrlStr: 'https://github.com/nuxeo/nuxeo-retention')
-  }
   environment {
     BRANCH_NAME = "${params.BRANCH}"
     BUILD_VERSION = "${params.BUILD_VERSION}"
